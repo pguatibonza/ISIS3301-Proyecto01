b09db00b697e2c8ac635a78a550ab50a243f31f8
Pablo <p.guatibonza@uniandes.edu.co>
2023-04-18 18:56:27 -0500

    variable sentimiento añadida

--- api/DataModel.py
+++ api/DataModel.py
@@ -4,6 +4,7 @@ class DataModel(BaseModel):
 
 # Estas varibles permiten que la librería pydantic haga el parseo entre el Json recibido y el modelo declarado.
     review_es: str
+    sentimiento:str
     
 
 #Esta función retorna los nombres de las columnas correspondientes con el modelo esxportado en joblib.
