041da14804976edfa2fc966ed3d6db2fb7fc01b5
Pablo <p.guatibonza@uniandes.edu.co>
2023-04-18 16:59:12 -0500

    creacion api

---
+++ api/main.py
@@ -0,0 +1,27 @@
+from typing import Optional
+
+from fastapi import FastAPI
+
+import pandas as  pd
+from joblib import load
+from DataModel import DataModel
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+   return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Optional[str] = None):
+   return {"item_id": item_id, "q": q}
+
+@app.post("/predict")
+def make_predictions(dataModel: DataModel):
+    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    df.columns = dataModel.columns()
+    model = load("../logistic_regression.joblib")
+    result = model.predict(df)
+    return result
