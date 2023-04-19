73c381504ea36da6a0fc39c1b015e4e386f95918
Juan José Osorio <j.osorioc@uniandes.edu.co>
2023-04-18 19:08:32 -0500

    Index más nuevo make predictions

--- api/PredictionModel.py
+++ api/PredictionModel.py
@@ -1,12 +1,15 @@
 from joblib import load
+import pandas as pd
+
 
 class Model:
 
-    def __init__(self,columns):
+    def __init__(self, columns):
         self.model = load("assets/logistic_regression.joblib")
 
     def make_predictions(self, data):
         result = self.model.predict(data)
-        return result
-    def retrain_model(self,data):
+        df = pd.DataFrame(result, columns=['predict'])
+
+    def retrain_model(self, data):
         return None
--- api/main.py
+++ api/main.py
@@ -1,33 +1,44 @@
+from io import StringIO
 from typing import Optional
 
-from fastapi import FastAPI, Request
-
+from fastapi import FastAPI, Request, UploadFile
+from PredictionModel import Model
 from fastapi.templating import Jinja2Templates
-import pandas as  pd
+import pandas as pd
 from joblib import load
 from DataModel import DataModel
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/")
 def read_root(request: Request):
-   return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-   return {"item_id": item_id, "q": q}
+    return {"item_id": item_id, "q": q}
+
 
 @app.post("/predict")
-def make_predictions(request : Request, dataModel: DataModel):
-    df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
-    df.columns = dataModel.columns()
-    model = load("assets/logistic_regression.joblib")
-    result = model.predict(df)
-    return result
+async def make_predictions(request: Request, file: UploadFile):
+    #  df = pd.DataFrame(dataModel.dict(), columns=dataModel.dict().keys(), index=[0])
+    #  df.columns = dataModel.columns()
+    #  model = load("assets/logistic_regression.joblib")
+    #  result = model.predict(df)
+    #  return result
+    contents = await file.read()
+    df = pd.read_csv(StringIO(contents.decode()))
+
+    model = Model()
+    predictions_dic = model.make_predictions(df)
+    predictions = predictions_dic['prediction']
+    return templates.TemplateResponse("index.html", {"request": request, "predictions": dict(predictions)})
+
 
 @app.post("/retrain")
 def retrain_model(dataModel: DataModel):
 
-   return None
+    return None
